05cca80acfd1b415107c9ec535ce575b97c0be5c
Iveta <quietbits@gmail.com>
2022-08-15 11:14:07 -0400

    Create account keys + ktfmt plugin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+import org.stellar.walletsdk.Wallet
+
 fun main() {
-    // TODO: add examples
+  // Create wallet instance with Horizon
+  val wallet = Wallet("https://horizon-testnet.stellar.org", "Test SDF Network ; September 2015")
+
+  // Create account, generate keypairs
+  //  val newAccountKeypair = wallet.create()
+  //  println(newAccountKeypair)
 }
--- src/main/kotlin/org/stellar/walletsdk/Wallet.kt
+++ src/main/kotlin/org/stellar/walletsdk/Wallet.kt
@@ -1,5 +1,22 @@
 package org.stellar.walletsdk
 
+import org.stellar.sdk.KeyPair
+import org.stellar.sdk.Network
+import org.stellar.sdk.Server
+import shadow.com.google.gson.annotations.SerializedName
+
 class Wallet(private val horizonUrl: String, private val horizonPassphrase: String) {
-    // TODO: Wallet code goes here
+  private val server = Server(this.horizonUrl)
+  private val network = Network(this.horizonPassphrase)
+
+  data class AccountKeypair(
+    @SerializedName("publicKey") val publicKey: String,
+    @SerializedName("secretKey") val secretKey: String
+  )
+
+  // Create account keys, generate new keypair
+  fun create(): AccountKeypair {
+    val keypair: KeyPair = KeyPair.random()
+    return AccountKeypair(keypair.accountId, String(keypair.secretSeed))
+  }
 }
