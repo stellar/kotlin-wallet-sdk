a8263c16ddbda571732492a53b1a7ea7d1f6af59
Gleb <gleb@stellar.org>
2022-11-04 16:46:49 -0700

    Add full SEP-24 example

--- examples/SEP-24/src/main/kotlin/org/stellar/walletsdk/Main.kt
+++ examples/SEP-24/src/main/kotlin/org/stellar/walletsdk/Main.kt
@@ -1,41 +1,100 @@
 package org.stellar.walletsdk
 
-import kotlinx.coroutines.runBlocking
+import java.math.BigDecimal
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.delay
 import org.stellar.sdk.KeyPair
 import org.stellar.sdk.Network
 import org.stellar.sdk.Server
 import org.stellar.sdk.Transaction
 
+// Setup main account that will fund new (user) accounts. You can get new key pair and fill it with
+// testnet tokens at
+// https://laboratory.stellar.org/#account-creator?network=test
 private val myAddress = "GCNVZJLJ5XZZALLZBR7BGUMGMF2KVZTBIGTU2A5KOPS2PPCZS6PV4LAD"
 private val myKey = "SDYGC4TW5HHR5JA6CB2XLTTBF2DZRH2KDPBDPV3D5TXM6GF7FBPRZF3I"
 
-fun main() {
+suspend fun main() {
+  // Create instance of server that allows to connect to Horizon
+  val server = Server("https://horizon-testnet.stellar.org")
   val wallet = Wallet()
+  // Generate new (user) account and fund it with 10 XLM from main account
   val account = KeyPair.random()
-  val tx = runBlocking { wallet.fund(myAddress, account.publicKeyString, "10") }
+  val tx = wallet.fund(myAddress, account.publicKeyString, "10")
 
+  // Sign with your main account's key and send transaction to the network
+  println("Registering new account")
   tx.sign(KeyPair.fromSecretSeed(myKey))
+  assert(wallet.submitTransaction(tx))
 
-  val res = runBlocking { wallet.submitTransaction(tx) }
+  val anchor = Anchor(server, Network.TESTNET)
 
-  val anchor = Anchor(Server("https://horizon-testnet.stellar.org"), Network.TESTNET)
+  // Get info from the anchor server
+  val info = anchor.getInfo("https://testanchor.stellar.org/.well-known/stellar.toml")
 
-  val info = runBlocking {
-    anchor.getInfo("https://testanchor.stellar.org/.well-known/stellar.toml")
+  // Get SEP-24 info
+  val servicesInfo = anchor.getServicesInfo("https://testanchor.stellar.org/sep24")
+
+  println("Info from anchor server: $info")
+  println("SEP-24 info from anchor server: $servicesInfo")
+
+  // Create add trustline transaction for an asset. This allows user account to receive trusted
+  // asset.
+  val addTrustline =
+    wallet.addAssetSupport(
+      account.publicKeyString,
+      "SRT",
+      "GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B"
+    )
+
+  // Sign and send transaction
+  println("Adding trustline")
+  addTrustline.sign(account)
+  assert(wallet.submitTransaction(addTrustline))
+
+  // Start interactive deposit
+  val deposit =
+    anchor.getInteractiveDeposit(
+      account.publicKeyString,
+      homeDomain = "testanchor.stellar.org",
+      assetCode = "SRT",
+      walletSigner = WalletSignerImpl(account)
+    )
+
+  // Request user input
+  println("Additional user info is required for the deposit, please visit: ${deposit.url}")
+
+  // Get transaction info
+  // val transaction = GET TRANSFER_SERVER_SEP0024/transaction
+
+  println("Waiting for tokens...")
+  // Wait for token to appear on user account
+  while (
+    server
+      .accounts()
+      .account(account.publicKeyString)
+      .balances
+      .filter { it.assetCode.isPresent && it.assetCode.get() == "SRT" }
+      .any { it.balance.toBigDecimal() <= BigDecimal.ZERO }
+  ) {
+    delay(5.seconds)
   }
 
-  val servicesInfo = runBlocking { anchor.getServicesInfo("https://testanchor.stellar.org/sep24") }
-
-  val resp = runBlocking {
+  // Start interactive withdrawal
+  val withdrawal =
     anchor.getInteractiveWithdrawal(
       account.publicKeyString,
       homeDomain = "testanchor.stellar.org",
       assetCode = "SRT",
       walletSigner = WalletSignerImpl(account)
     )
-  }
 
-  print(resp)
+  // Request user input
+  println("Additional user info is required for the withdrawal, please visit: ${withdrawal.url}")
+
+  // Wait for user input
+
+  // Send transaction with transfer
 }
 
 val KeyPair.publicKeyString: String
@@ -54,6 +113,9 @@ class WalletSignerImpl(private val keyPair: KeyPair) : WalletSigner {
     transactionString: String,
     networkPassPhrase: String
   ): Transaction {
-    TODO("Not yet implemented")
+    val txn =
+      Transaction.fromEnvelopeXdr(transactionString, Network(networkPassPhrase)) as Transaction
+    txn.sign(keyPair)
+    return txn
   }
 }
