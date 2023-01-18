ec9e08a217ba94ceff2525adbb773e2780e5c46f
Gleb <gleb@stellar.org>
2023-01-18 09:24:42 -0800

    Small fix of recovery server data class (#62)

--- wallet-sdk/src/main/kotlin/org/stellar/walletsdk/recovery/Data.kt
+++ wallet-sdk/src/main/kotlin/org/stellar/walletsdk/recovery/Data.kt
@@ -9,10 +9,20 @@ import org.stellar.walletsdk.auth.WalletSigner
 
 @Serializable internal data class AuthSignature(val signature: String)
 
+/**
+ * Recovery server configuration
+ *
+ * @constructor Create empty Recovery server
+ * @property endpoint main endpoint (root domain) of SEP-30 recovery server. E.g.
+ *   `https://testanchor.stellar.org`
+ * @property authEndpoint SEP-10 auth endpoint to be used. Should be in format
+ *   `<https://domain/auth>`. E.g. `https://testanchor.stellar.org/auth`)
+ * @property homeDomain is a SEP-10 home domain. E.g. `testanchor.stellar.org`
+ * @property walletSigner optional [WalletSigner] used to sign authentication
+ */
 data class RecoveryServer(
   val endpoint: String,
   val authEndpoint: String,
-  val stellarAddress: String,
   val homeDomain: String,
   val walletSigner: WalletSigner? = null
 )
