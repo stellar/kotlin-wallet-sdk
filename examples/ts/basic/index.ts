e5b9e6352bef7851ac9a5bca614b480c6ec5ab64
Iveta <quietbits@users.noreply.github.com>
2023-02-09 19:43:41 -0500

    Basic TS example (#69)
    * Basic TS example
    
    * Add package.json

---
+++ examples/ts/basic/index.ts
@@ -0,0 +1,18 @@
+// A very basic example of how to use TypeScript Wallet SDK
+import * as TypeScriptWalletSdk from "kotlin-wallet-sdk";
+
+// TODO: This will change, `org.stellar.walletsdk` won't be required
+const WalletSdk = TypeScriptWalletSdk.org.stellar.walletsdk;
+
+function basicWallet() {
+  const config = new WalletSdk.StellarConfiguration();
+  const wallet = new WalletSdk.Wallet(config);
+  const anchor = wallet.anchor("https://testanchor.stellar.org");
+  const info = anchor.getInfo();
+
+  console.log("info: ", info);
+}
+
+basicWallet();
+
+// Run in Node `npm run basic`
