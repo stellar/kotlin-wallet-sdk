2d4be7c407f162f3eec39751ffe069505076c35c
Gleb <gleb@stellar.org>
2023-03-07 09:59:18 -0800

    WAL-688: Add WalletSigner documentation (#76)

--- examples/documentation/build.gradle.kts
+++ examples/documentation/build.gradle.kts
@@ -1,3 +1,17 @@
-dependencies { implementation(project(":wallet-sdk")) }
+// The alias call in plugins scope produces IntelliJ false error which is suppressed here.
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.kotlin.serialization)
+}
 
 sourceSets.main { java.srcDirs("src") }
+
+dependencies {
+    implementation(project(":wallet-sdk"))
+
+    implementation(libs.kotlin.serialization.json)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.serialization.json)
+    implementation(libs.ktor.client.okhttp)
+}
